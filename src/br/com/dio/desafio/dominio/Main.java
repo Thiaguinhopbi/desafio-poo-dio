c24b65a30a15add20fec3d424654fe68583733e4
thiagodesouza <tdsrogerio@gmail.com>
2023-01-28 19:44:51 -0300

    Conteudo

--- src/br/com/dio/desafio/dominio/Main.java
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -14,8 +14,8 @@ public class Main {
         curso2.setDescricao("Descrição do curso  JS ");
         curso2.setCargaHoraria(4);
 
-        Mentoria mentoria1 = new Mentoria();
 
+        Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("Descrição Mentoria Java");
         mentoria1.setData(LocalDate.now());
